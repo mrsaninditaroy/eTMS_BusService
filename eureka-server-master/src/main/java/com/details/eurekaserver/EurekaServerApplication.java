dd456a909db086339022ca3cc3327e66d54f4789
AninditaRoy20011985 <mrsaninditaroy@gmail.com>
2023-08-28 12:24:01 +0530

    Eureka Server files

--- eureka-server-master/src/main/java/com/details/eurekaserver/EurekaServerApplication.java
+++ eureka-server-master/src/main/java/com/details/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.code22.eurekaserver;
+package com.details.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server-master/src/test/java/com/details/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server-master/src/test/java/com/details/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.code22.eurekaserver;
+package com.details.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
